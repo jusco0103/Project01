59f3ce017f90a544afe5592c320389e1a5a214d8
jusco0103 <board0103.swim.run@gmail.com>
2018-01-26 16:47:37 +0900

    Saisho

---
+++ tekitou/TestAlart.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UIKit;
+namespace tekitou
+{
+    public class TestAlart : UIViewController
+    {
+        public TestAlart()
+        {
+            var ac = UIAlertController.Create("確認", "よろしいですか？", UIAlertControllerStyle.Alert);
+            var action = UIAlertAction.Create("OK", UIAlertActionStyle.Default, null);
+            ac.AddAction(action);
+            PresentViewController(ac, true, null);
+
+        }
+    }
+}
